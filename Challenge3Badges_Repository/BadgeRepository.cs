57252c05677b0bc28570e9e5324b18f04a64ab5c
imrazave <imrazave@gmail.com>
2020-12-15 13:07:27 -0500

    eighth commit

--- Challenge3Badges_Repository/BadgeRepository.cs
+++ Challenge3Badges_Repository/BadgeRepository.cs
@@ -16,6 +16,30 @@ namespace Challenge3Badges_Repository
             _badgeDictionary.Add(badgeId, newbadge);
         }
 
+        public bool AddDoorToBadge(int badgeId, string doorName)
+        {
+            //Find the badge
+            Badge badge = GetBadgeByID(badgeId);
+
+            if (badge == null)
+            {
+                return false;
+            }
+
+            int doorCount = badge.DoorName.Count;
+
+            //Add the door to the badge
+            badge.DoorName.Add(doorName);
+            
+            if (doorCount < badge.DoorName.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         //Read
         public Dictionary<int, Badge> GetBadges()
         {
@@ -23,17 +47,85 @@ namespace Challenge3Badges_Repository
         }
 
         //Update
-        public bool UpdateExistingBadge()
+        public bool UpdateExistingBadge(int existingBadgeId, Badge updatedBadge)
         {
-            
+            //Find the badge
+            Badge existingBadge = GetBadgeByID(existingBadgeId);
+
+            //Update the badge
+            if (existingBadge != null)
+            {
+                existingBadge.DoorName = updatedBadge.DoorName;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         //Delete
-        public bool RemoveAllDoorsFromBadge()
+        public bool RemoveAllDoorsFromBadge(int existingBadgeId)
         {
+            Badge badge = GetBadgeByID(existingBadgeId);
+
+            if(badge == null)
+            {
+                return false;
+            }
+
+            int doorCount = badge.DoorName.Count;
+
+            badge.DoorName.Clear();
+
+            if (doorCount > badge.DoorName.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
         }
 
+        public bool RemoveOneDoorFromBadge(int existingBadgeId, string doorName)
+        {
+            Badge badge = GetBadgeByID(existingBadgeId);
 
+            if (badge == null)
+            {
+                return false;
+            }
+
+            int doorCount = badge.DoorName.Count;
+
+            badge.DoorName.Remove(doorName);
+
+            if (doorCount > badge.DoorName.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //Helper Method
+        public Badge GetBadgeByID(int badgeId)
+        {
+            foreach (KeyValuePair<int, Badge> entry in _badgeDictionary)
+            {
+                if (entry.Key == badgeId)
+                {
+                    return entry.Value;
+                }
+            }
+
+            return null;
+        }
     }
 }
